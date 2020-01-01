e02c8e190b89e494c60752c92e922b410d65bdda
jcable <julian.cable@yahoo.com>
2020-01-01 22:59:52 +0000

    configure for running backend in root of repo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { ApolloServer, gql } = require('apollo-server');
 const fs = require('fs')
 
-const typeDefs = gql(fs.readFileSync('../schema.graphql', 'utf8'))
-const resolvers = require('./resolvers')
+const typeDefs = gql(fs.readFileSync('schema.graphql', 'utf8'))
+const resolvers = require('./drupal-backend/resolvers')
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
