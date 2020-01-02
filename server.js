5f0c843a814dd8e510cadbcf87ae87a4a9942938
jcable <julian.cable@yahoo.com>
2020-01-02 12:55:11 +0000

    update to use npm mysql2 connection pooling and promises

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server');
 const fs = require('fs')
+const mysql = require('mysql2');
 
-const typeDefs = gql(fs.readFileSync('schema.graphql', 'utf8'))
-const resolvers = require('./drupal-backend/resolvers')
-const server = new ApolloServer({ typeDefs, resolvers });
+const typeDefs = gql(fs.readFileSync('./schema.graphql', 'utf8'))
+const resolvers = require('.drupal_backend/resolvers')
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+const pool  = mysql.createPool({
+  connectionLimit : 4,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+  host: process.env.MYSQL_HOST
+});
+
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: () => ({db: pool.promise() })
+});
+
+server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
