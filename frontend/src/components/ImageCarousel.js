2e0d2399425048643d6f1a3f3808a74bc4a5f16c
jcable <julian.cable@yahoo.com>
2020-01-04 12:10:29 +0000

    fix bug when no pictures

--- frontend/src/components/ImageCarousel.js
+++ frontend/src/components/ImageCarousel.js
@@ -1,53 +1,56 @@
 import React from 'react';
-import { CarouselProvider, Image, Slider, Slide, Dot, ButtonFirst, ButtonLast  } from 'pure-react-carousel';
+import { CarouselProvider, Image, Slider, Slide, Dot, ButtonFirst, ButtonLast } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { Container, Image as ReactImage, Icon, Grid } from 'semantic-ui-react';
-//className='fixedAspect' style={{bottom:10,left:10,position:"absolute"}}>
-const Thumb = ({image}) => {
-    return (<ReactImage src={image.uri} size="tiny"/>)
+
+const Thumb = ({ image }) => {
+    return (<ReactImage src={image.uri} size="tiny" />)
 }
 
-const ImageCarousel = ({images}) => {
-    console.log('images', images);
-      return (
+const ImageCarousel = ({ images }) => {
+    if(!images) {
+        return "We don't have any pictures of this boat. We'd love to have some!"
+    }
+
+    return (
         <CarouselProvider className="Br-tall"
-          naturalSlideWidth={100}
-          naturalSlideHeight={100}
-          totalSlides={images?images.length:0}
+            naturalSlideWidth={100}
+            naturalSlideHeight={100}
+            totalSlides={images ? images.length : 0}
         >
-        <Slider>
-            {
-                (images)?
-                    images.map((image, i) =>(
-                        <Slide index={i} key={i}>
-                        <Image tag="div" src={image.uri} children={
-                            (<Container className="Br-copyright-outer">
-                                <Container className="Br-copyright-inner">
-                                    <Icon name="copyright outline"/>{image.copyright}
-                                </Container>
-                            </Container>)
-                        }>
-                        </Image>   
-                        </Slide>
-                    ))
-                :''
-            }
-        </Slider> 
-        <br/>       
-        <Grid >
-        <Grid.Row centered verticalAlign="middle">
+            <Slider>
+                {
+                    (images) ?
+                        images.map((image, i) => (
+                            <Slide index={i} key={i}>
+                                <Image tag="div" src={image.uri} children={
+                                    (<Container className="Br-copyright-outer">
+                                        <Container className="Br-copyright-inner">
+                                            <Icon name="copyright outline" />{image.copyright}
+                                        </Container>
+                                    </Container>)
+                                }>
+                                </Image>
+                            </Slide>
+                        ))
+                        : ''
+                }
+            </Slider>
+            <br />
+            <Grid >
+                <Grid.Row centered verticalAlign="middle">
 
-        <ButtonFirst><Icon size="big" name="angle double left"/></ButtonFirst>
-        {
-            images.map((image, index)=>{
-                return (<Dot key={index} slide={index}><Thumb image={image}/></Dot>)
-            })
-        }
-        <ButtonLast><Icon size="big" name="angle double right"/></ButtonLast>
-        </Grid.Row>
-        </Grid>
-    </CarouselProvider>
-      );
-  }
+                    <ButtonFirst><Icon size="big" name="angle double left" /></ButtonFirst>
+                    {
+                        images.map((image, index) => {
+                            return (<Dot key={index} slide={index}><Thumb image={image} /></Dot>)
+                        })
+                    }
+                    <ButtonLast><Icon size="big" name="angle double right" /></ButtonLast>
+                </Grid.Row>
+            </Grid>
+        </CarouselProvider>
+    );
+}
 
-  export default ImageCarousel
+export default ImageCarousel
