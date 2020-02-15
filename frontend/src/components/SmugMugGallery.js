8ee59984ff85b715ac6bb197c2a99fd7bd68a1e4
jcable <julian.cable@yahoo.com>
2020-02-15 22:39:13 +0000

    wip

--- frontend/src/components/SmugMugGallery.js
+++ frontend/src/components/SmugMugGallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import Iframe from 'react-iframe'
 
 const url = 'https://juliancable.smugmug.com/frame/slideshow'
@@ -7,11 +7,34 @@ const queryString = 'autoStart=1&captions=1&navigation=1&playButton=1&randomize=
 const map = {
     2148: 'smhdQr'
 };
+
 const SmugMugGallery = ({ id }) => {
+
+    const [key, setKey] = useState();
+
+    useEffect(() => {
+        const url = 'https://api.smugmug.com/api/v2/album!search'
+            +'?Scope=https://api.smugmug.com/api/v2/folder/user/juliancable/Boats'
+            +'&Text='+id
+
+        fetch(
+            url,
+            {
+              method: "GET",
+              headers: new Headers({ Accept: "application/json" })
+            }
+          )
+          .then(res => res.json())
+            .then(response => {
+                console.log(response);
+                setKey(map[id]);
+                })
+            .catch(error => console.log(error));
+    }, [id])
 
     return (
         <Iframe
-        url={`${url}?key=${map[id]}&${queryString}`}
+        url={`${url}?key=${key}&${queryString}`}
         width="800" 
         height="600" 
         frameborder="no" 
